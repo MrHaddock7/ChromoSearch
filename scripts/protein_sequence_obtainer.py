07d35408d8a8df2284b616a776b71419bbfe0d13
MrHaddock7 <113512371+MrHaddock7@users.noreply.github.com>
2024-08-08 11:59:16 +0200

    Commented

--- scripts/protein_sequence_obtainer.py
+++ scripts/protein_sequence_obtainer.py
@@ -5,23 +5,23 @@ logger = logging.getLogger(__name__)
 
 def name_and_sequence_pair(input_genome_fasta, alignment_references, input_database_fasta, blastpsw=True):
     logger.debug('Entering pname_and_sequence_pair function')
-    def parse_fasta_file(file_path):
+    def parse_fasta_file(file_path): # Input, path to FASTA file you want to parse
         logger.debug('Entering parse_fasta_file function')
         try:
             sequences = []
             with open(file_path, 'r') as f:
                 lines = f.readlines()
-                current_name = None
+                current_name = None 
                 current_sequence = []
                 for line in lines:
                     line = line.strip()
-                    if line.startswith('>'):
+                    if line.startswith('>'): # Adding the name of the protein
                         if current_name is not None:
                             sequences.append((current_name, ''.join(current_sequence)))
                         current_name = line[1:].split()[0]  # Extracting the protein name
                         current_sequence = []
                     else:
-                        current_sequence.append(line)
+                        current_sequence.append(line) # Adding the protein sequence
                 # Append the last sequence
                 if current_name is not None and current_sequence:
                     sequences.append((current_name, ''.join(current_sequence)))
@@ -36,21 +36,22 @@ def name_and_sequence_pair(input_genome_fasta, alignment_references, input_datab
         fasta_file = input_genome_fasta
         fasta_file2 = input_database_fasta
 
-        sequences = parse_fasta_file(fasta_file)
-        sequences2 = parse_fasta_file(fasta_file2)
+        sequences = parse_fasta_file(fasta_file) # Parsing the genome 
+        sequences2 = parse_fasta_file(fasta_file2) # Parsing the database
 
         df = pd.DataFrame(sequences, columns=['Protein Name', 'Sequence'])
         df2 = pd.DataFrame(sequences2, columns=['Protein Name', 'Sequence'])
         df.set_index('Protein Name', inplace=True)
         df2.set_index('Protein Name', inplace=True)
 
-        final_list = []
+        final_list = [] 
 
         sorted_output_df = pd.read_csv(alignment_references)
 
-        ## Perform the SW algorithm based on the highest scoring results from BlastP
+        ## Output pairs based on the highest scoring results from BlastP
 
         if blastpsw:
+            logger.info('Paring from BlastP results')
 
             try:
                 for i in range(len(sorted_output_df)):
@@ -70,13 +71,14 @@ def name_and_sequence_pair(input_genome_fasta, alignment_references, input_datab
             except Exception as e:
                 print(f"An unexpected error occurred: {e}")
 
-        ## Perform the BlastP algorithm based on all protein candidate-protein database combinations.
 
+        ## Outputs ALL possible pairs of genes and entries in the database.
         ## NOTE! This may be VERY 
         ## computationally intensive for larger databases and/or larger protein
         ## candidate families.
 
         else:
+            logger.info('Paring ALL possible combinations of genes in query and database')
 
             final_list = []
             try:
@@ -97,6 +99,3 @@ def name_and_sequence_pair(input_genome_fasta, alignment_references, input_datab
         logging.error(f'Error in name_and_sequence_pair function: {ex}')
     logger.debug('Exiting pname_and_sequence_pair function')
     return final_list
-
-# if __name__ == '__main__':
-#     name_and_sequence_pair()
